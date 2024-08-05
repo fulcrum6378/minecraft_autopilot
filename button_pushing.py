934b765f24a98b711ea1ac28aaf5de0afdbf79ae
Mahdi Parastesh <fulcrum1378@gmail.com>
2024-08-05 18:04:22 +0330

    2024.08.05 - 2 (win32functions.SendInput is not suitable)

--- button_pushing.py
+++ button_pushing.py
@@ -1,18 +1,17 @@
 import copy
 import ctypes
-import locale
 import time
 import warnings
 
 # noinspection PyUnresolvedReferences
 import pywintypes
-import six
-import win32api
 # noinspection PyUnresolvedReferences
 import win32con
 import win32gui
-from pywinauto import win32functions, Application, keyboard, win32defines
+from pywinauto import win32functions, Application, keyboard, win32structures
 from pywinauto.controls.hwndwrapper import DialogWrapper
+from pywinauto.keyboard import KeyAction
+from win32.lib.win32con import KEYEVENTF_KEYUP, INPUT_KEYBOARD, KEYEVENTF_UNICODE
 
 
 # open notepad and make it push a button continually...
@@ -137,89 +136,34 @@ def send_my_keys(w,
         win32functions.AttachThreadInput(target_thread_id, current_thread_id, False)
 
 
-def type_my_keys(
-        w,
-        keys,
-        pause=None,
-        with_spaces=False,
-        with_tabs=False,
-        with_newlines=False,
-        turn_off_numlock=True,
-        set_foreground=True,
-        vk_packet=True):
-    """
-    Type keys to the element using keyboard.send_keys
-
-    This uses the re-written keyboard_ python module where you can
-    find documentation on what to use for the **keys**.
-
-    .. _keyboard: pywinauto.keyboard.html
-    """
-    w.verify_actionable()
-    friendly_class_name = w.friendly_class_name()
-
-    if pause is None: pause = .01
-
-    # if set_foreground:
-    #    w.set_focus()
-
-    # attach the Python process with the process that self is in
-    if w.element_info.handle:
-        window_thread_id = win32functions.GetWindowThreadProcessId(w.handle, None)
-        win32functions.AttachThreadInput(win32functions.GetCurrentThreadId(), window_thread_id, win32defines.TRUE)
-        # TO-DO: check return value of AttachThreadInput properly
-    else:
-        # TO-DO: UIA stuff maybe
-        pass
-
-    if isinstance(keys, six.text_type):
-        aligned_keys = keys
-    elif isinstance(keys, six.binary_type):
-        aligned_keys = keys.decode(locale.getpreferredencoding())
-    else:
-        # convert a non-string input
-        aligned_keys = six.text_type(keys)
-
-    # Play the keys to the active window
-    keys = keyboard.parse_keys(
-        keys, with_spaces, with_tabs, with_newlines,
-        vk_packet=vk_packet)
-
-    for k in keys:
-        k.run()
-        time.sleep(pause)
-
-    # detach the python process from the window's process
-    if w.element_info.handle:
-        win32functions.AttachThreadInput(win32functions.GetCurrentThreadId(), window_thread_id, win32defines.FALSE)
-        # TO-DO: check return value of AttachThreadInput properly
-    else:
-        # TO-DO: UIA stuff
-        pass
-
-    w.wait_for_idle()
-
-    # w.actions.log('Typed text to the ' + friendly_class_name + ': ' + aligned_keys)
-    return w
-
-
 app = Application(backend="uia").start("notepad.exe")
 dw: DialogWrapper = app.top_window().wrapper_object()
 # dw.set_focus()
-# print(dw.handle)
-type_my_keys(dw, "FUCK")
-# time.sleep(1)
-# win32gui.SendMessage(dw.handle, win32con.WM_KEYDOWN, win32con.VK_ESCAPE, 0)
-# win32functions.PostMessage(dw.handle, win32con.WM_CHAR, ord('F'), 0)
-# win32gui.SendMessage(dw.handle, win32con.WM_KEYDOWN, 0x41, 0x1E0001)
-# win32api.keybd_event()
+keys: list[KeyAction] = [KeyAction('F', True, False), ]  # KeyAction('U'), KeyAction('C'), KeyAction('K'),
+for k in keys:
+    actions = 1
+    if k.up and k.down: actions = 2
+    inputs = (win32structures.INPUT * actions)()
+
+    for inp in inputs:
+        inp.type = INPUT_KEYBOARD
+        inp.ki.wVk = 0
+        inp.ki.wScan = ord(k.key)
+        inp.ki.dwFlags |= KEYEVENTF_UNICODE
+        # it seems to return 0 every time, but it's required by MSDN specification so call it just in case
+        inp.ki.dwExtraInfo = win32functions.GetMessageExtraInfo()
+    # if we are releasing - then let it up
+    if k.up: inputs[-1].ki.dwFlags |= KEYEVENTF_KEYUP
+
+    num_inserted_events = win32functions.SendInput(len(inputs), ctypes.byref(inputs),
+                                                   ctypes.sizeof(win32structures.INPUT))
+    if num_inserted_events != len(inputs):
+        raise RuntimeError('SendInput() inserted only ' + str(num_inserted_events) +
+                           ' out of ' + str(len(inputs)) + ' keyboard events')
+    time.sleep(.01)
+win32functions.WaitGuiThreadIdle(dw.handle)
 time.sleep(1)
-# win32gui.SendMessage(dw.handle, win32con.WM_KEYUP, win32con.VK_ESCAPE, 0)
-# win32functions.PostMessage(dw.handle, win32con.WM_CHAR, ord('F'), 0)
-# dw.send_message(win32con.WM_CHAR, ord('F'), 0)
 # send_my_keys(dw.handle, "F")
-# win32functions.PostMessage(dw.handle, win32con.WM_CHAR, ord('F'), 0)
-# win32gui.SendMessage(dw.handle, win32con.WM_KEYUP, 0x41, 0x1E0001)
 time.sleep(2)
 # dw.type_keys("%FX")
 win32gui.SendMessage(dw.handle, win32con.WM_CLOSE)
